85e36dd71401bc4b11515870db20b34c7faf93c2
sandula-sanchana <sandulasanchana@gmail.com>
2025-12-04 07:55:13 +0530

    add tracking page and all routing done

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -2,7 +2,9 @@ import { Routes, Route } from 'react-router'
 import { HomePage } from './pages/HomePage'
 import { Checkout } from './pages/Checkout'
 import { OrderPage } from './pages/OrderPage'
+import { TrackingPage } from './pages/TrackingPage'
 import './App.css'
+
 
 function App() {
 
@@ -11,8 +13,9 @@ function App() {
     <>
       <Routes>
         <Route index element={<HomePage />} />
-        <Route path="checkout" element={<Checkout />} />
-        <Route path="order" element={<OrderPage />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/order" element={<OrderPage />} />
+        <Route path="/tracking" element={<TrackingPage />} />
       </Routes>
 
     </>
